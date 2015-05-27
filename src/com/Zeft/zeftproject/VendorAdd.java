b3bc21b8f8821a62467ff969689de1c6e29a3306
SaadHazem22 <saadajaj35@gmail.com>
2015-05-27 11:47:12 +0300

    new

--- src/com/Zeft/zeftproject/VendorAdd.java
+++ src/com/Zeft/zeftproject/VendorAdd.java
@@ -13,22 +13,5 @@ public class VendorAdd extends Activity {
 		setContentView(R.layout.activity_vendor_add);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.vendor_add, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
